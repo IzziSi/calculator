166b53c5547d84771dfe6888d6dc3cc3f8f9a3d9
Sabrina Graziano <izzi.sabri@gmail.com>
2018-04-24 11:32:55 -0400

    rewrote the entire script to use arrays instead of toggling between variables

--- js/script.js
+++ js/script.js
@@ -1,63 +1,47 @@
-var inputOne;
-var inputTwo;
-var inputThree;
-var results = 0;
-//var beforeResults=0;
-
+var input = [];
+var operator = [];
+var tempInput = '';
+var tempOperator = '';
+var results = '';
 
 function inputNumber(number) {
-if (inputTwo=== undefined) {
-    inputOne += number;
-    document.getElementById('input').value = inputOne;
+    if (tempOperator === '') {
+        tempInput += number;
+    } else {
+        operator.push(tempOperator);
+        tempOperator = '';
+        tempInput += number;
+    }
+    document.getElementById('input').value = tempInput;
 }
-else {
-    inputThree += number;
-  //  beforeResults = parseInt(inputOne) + inputTwo + parseInt(inputThree);
-    //document.getElementById('input').value = beforeResults;
-}
-}
-
 
 function inputOperator(operator) {
-    inputTwo = operator;
-    //beforeResults = parseInt(inputOne) + parseInt(inputTwo);
-    //document.getElementById('input').value = beforeResults;
+    if (tempInput === '') {
+        tempOperator += operator;
+    } else {
+        input.push(tempInput);
+        tempInput = '';
+        tempOperator += operator;
     }
-    
-function calculateInputs() {
-switch (inputTwo) {
-    case '/':
-    results = parseInt(inputOne) / parseInt(inputThree);
-    document.getElementById('input').value = results;
-    break;
-    case '*':
-    results = parseInt(inputOne) * parseInt(inputThree);
-    document.getElementById('input').value = results;
-    break;
-    case '-':
-    results = parseInt(inputOne) - parseInt(inputThree);
-    document.getElementById('input').value = results;
-    break;
-    case '+':
-    results = parseInt(inputOne) + parseInt(inputThree);
-    document.getElementById('input').value = results;
-    break;
-    case '%':
-    results = parseInt(inputOne) % parseInt(inputThree);
-    document.getElementById('input').value = results;
-    break;
-    default:
-    results = 0;
-    document.getElementById('input').value = results;
-}
+    document.getElementById('input').value = tempOperator;
 }
 
-function clear() {
-inputOne = undefined;
-inputTwo = undefined;
-inputThree = undefined;
-results = 0;
-//beforeResults=0;
-}
 
-document.getElementById('input').value = results;
+//Does not work - fix
+function calculateInputOperator() {
+    if (tempInput != '') {
+        input.push(tempInput);
+        tempInput = '';
+    }
+    if (tempOperator != '') {
+        operator.push(tempOperator);
+        tempOperator = '';
+    }   
+    for (var i = 0; i < input.length; i++) {
+        results += parseInt(input.shift());
+        if (operator[i] != undefined) {
+            results += operator.shift();
+        }
+        document.getElementById('input').value = eval(results);
+    }
+}
---
+++ js/test.js
@@ -0,0 +1,6 @@
+var test = "cow";
+var array = [];
+
+array.push(test);
+
+console.log(array[1]);
